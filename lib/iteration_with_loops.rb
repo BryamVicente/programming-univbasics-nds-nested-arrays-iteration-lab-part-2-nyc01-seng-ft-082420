c252a7c545ae7af4d777b4ae3385a3414db3a094
Bryam Vicente <vicentebryam@gmail.com>
2020-08-15 15:45:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,14 +1,24 @@
 def find_min_in_nested_arrays(src)
+# count = 0
+#   solution_answer = []
+#   sorted_array = []
+#   src.each do |little_array|
+#     sorted_array << little_array.sort
+#   end
+words = []
+sorted_array = []
 count = 0
-  solution_answer = []
-  sorted_array = []
-  src.each do |little_array|
-    sorted_array << little_array.sort
+src.each do |items|
+  items.each do |item|
+    if item.is_a?(String)
+      words << item
+    end
   end
+end
 
   while count < sorted_array.count do
   inner_count = 0
-  minimum_amount = 50
+  minimum_amount = ""
   while inner_count < sorted_array.count do
     if sorted_array[count][inner_count] < minimum_amount
       minimum_amount = sorted_array[count][inner_count]
@@ -20,5 +30,5 @@ count = 0
   solution_answer << minimum_amount
   count += 1
   end
-  solution_answer
+words
 end
